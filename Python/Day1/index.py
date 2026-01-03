2b1433bd3a158f5cb3b5bbc8684d3e05f2224d1f
rajnandinitikkal <rajnandinitikkal@gmail.com>
2026-01-03 18:24:48 +0530

    add new code for day1 practice

--- Python/Day1/index.py
+++ Python/Day1/index.py
@@ -1,1 +1,27 @@
 print("Hello world")
+
+# for the ' use double quotation on side and then single quotation inside for showing o/p
+print("Tushar's Laptop")
+
+# if you want to use " " in o/p statement then use this combination
+print('Rituja\'s "Laptop" ')
+print('Rutuja "Laptop" ')
+
+# \n is used for new line 
+#  in this instead of nutan this line only prints utan bcz \n is used for new line 
+print('tushar:\nutan')
+
+# so we use r in the beginning of statement it will print what you want 
+# for e.g in this we want to print path but what if document name start with "n" that's why we use r in front of statement
+print(r'c:\docs\navin')
+print(r'tushar:\nutan')
+
+# Operations
+print(4+5)
+print(5*6)
+print(11 - 6)
+print(5 % 4)
+
+print('Tushar + Rutuja')
+
+
--- Python/Day1/readme.md
+++ Python/Day1/readme.md
@@ -4,6 +4,17 @@
 
 ### For Day 1 start with basic concepts
 
+### What is Python:
+- Python is a high-level, interpreted, and object-oriented programming language known for its simplicity and readability.
+- It was created by Guido van Rossum and first released in 1991.
+
+* Key Features
+- Easy to read and write
+- Interpreted Language
+- Object Oriented
+- Cross Platform
+- Versatile and Extensible
+
 Learning from 
 https://www.youtube.com/watch?v=DWgzHbglNIo&list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3&index=4
 
